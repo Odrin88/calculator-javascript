071c6ef974a1d7bcbec080977160e7751aa765e9
Odrin88 <67546924+Odrin88@users.noreply.github.com>
2020-10-06 23:28:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,143 @@
-/* let money = prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+'use strict';
+
+let startBtn = document.getElementById("start"),
+	budgetValue = document.getElementsByClassName('budget-value')[0],
+	dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+	levelValue = document.getElementsByClassName('level-value')[0],
+	expensesValue = document.getElementsByClassName('expenses-value')[0],
+	optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+	incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+
+	expensesItem = document.getElementsByClassName('expenses-item'),
+	expensesBtn = document.getElementsByTagName('button')[0],
+	optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+	incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+let money, time;
+
+startBtn.addEventListener('click', function() {
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+    money = +prompt ("Ваш бюджет на месяц?", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = prompt ("Ваш бюджет?", ""); 
+    }
+    appData.budget = money;
+    appData.timeData = time;
+    budgetValue.textContent = money.toFixed();
+    yearValue.value = new Date(Date.parse(time)).getFullYear();
+    monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
+    dayValue.value = new Date(Date.parse(time)).getDate();
+}); 
+
+expensesBtn.addEventListener('click', function() {
+    let sum = 0;
+
+    for (let i = 0; i < expensesItem.length; i++) {
+        let a = expensesItem[i].value,
+            b = expensesItem[++i].value;
+    
+        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+            console.log ("Все верно");
+            appData.expenses[a] = b;
+            sum += +b;
+        } else {
+            i = i - 1;
+        }
+    
+    }
+    expensesValue.textContent = sum;
+});
+
+optionalExpensesBtn.addEventListener('click', function(){
+    for (let i = 0; i <= optionalExpensesItem.length; i++) {
+        let opt = optionalExpensesItem[i].value;
+        appData.optionalExpenses[i] = opt;
+        optionalExpensesValue.textContent += appData.optionalExpenses[i] + ' '; 
+        
+    }
+});
+
+countBtn.addEventListener('click', function() {
+
+    if (appData.budget != undefined) {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        dayBudgetValue.textContent = appData.moneyPerDay;
+        
+        if (appData.moneyPerDay < 100) {
+            levelValue.textContent = "Это минимальный уровень достатка!";
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            levelValue.textContent = "Это средний уровень достатка!";
+        } else if (appData.moneyPerDay > 2000) {
+            levelValue.textContent = "Это высокий уровень достатка!";
+        } else {
+            levelValue.textContent = "Ошибочка...!";
+        } 
+    } else {
+        dayBudgetValue.textContent = "Ошибочка...!";
+    }
+    
+});
+
+incomeItem.addEventListener('input', function() {
+    let items = incomeItem.value;
+    appData.income = items.split(", ");
+    incomeValue.textContent = appData.income;
+});
+    
+checkSavings.addEventListener('click', function() {
+    if (appData.savings == true) {
+        appData.savings == false;
+    } else {
+        appData.savings == true;
+    }
+});
+
+
+sumValue.addEventListener('input', function() {
+    if (appData.savings = true) {
+        let sum = +sumValue.value,
+            percent = +percentValue.value;
+
+            appData.monthIncome = sum/100/12*percent;
+            appData.yearIncome = sum/100*percent;
+
+            monthSavingsValue.textContent = appData.monthIncome.toFixed(1);
+            yearSavingsValue.textContent = appData.yearIncome.toFixed(1);
+    }
+});
+
+percentValue.addEventListener('input', function() {
+    if (appData.savings = true) {
+        let sum = +sumValue.value,
+            percent = +percentValue.value;
+
+            appData.monthIncome = sum/100/12*percent;
+            appData.yearIncome = sum/100*percent;
+
+            monthSavingsValue.textContent = appData.monthIncome.toFixed(1);
+            yearSavingsValue.textContent = appData.yearIncome.toFixed(1);
+    }
+});
+
 
 let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false     
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
-
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-
-alert(appData.budget / 30); */
-
-let num = 50;
-if (num < 49) {
-    console.log('Неверно')
-} else if (num > 100) {
-    console.log('Много')
-} else {
-    console.log('Верно')
-};
-
-(num == 50) ? console.log("Верно") : console.log("НеВерно"); 
-
-switch (num) {
-    case num < 49:
-        console.log('Неверно');
-        break;
-    case num < 100:    
-        console.log('Много');
-        break;
-    case num < 80:    
-        console.log('Все еще Много');
-        break;
-    case 50:    
-        console.log('Верно');
-        break;
-    default:
-        console.log('Что то пошло не так');
-        break;
-}
-
-
-let num = 50;
-while (num < 55) {
-    console.log(num);
-    num++;
-}
-
-let num = 50;
-do {
-    console.log(num);
-    num++;
-}
-while (num < 50);
-
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        break
-    }
-    console.log(i)
-}
